a2ba30339b2436aa9b21d42a73d1c0571a750c07
Manhal-Baban <manhal.baban@gmail.com>
2025-03-28 13:04:44 +0200

    01_viikko

--- 01_viikko/js/skripti.js
+++ 01_viikko/js/skripti.js
@@ -1,2 +1,30 @@
 console. log ("Tämä on Javascript-testi.");
 alert("Moi");
+
+const muutuja ="hello world";
+
+function funktio () {
+  let p = 5*6;
+  let x, y, z;
+  x = 5
+  y = 5
+  z = x + y;
+
+  console.log ("this is z: ", z);
+
+
+let age = 33;
+let dob = 1991;
+
+let current_year = age + dob;
+
+console.log("this year is", current_year);
+
+
+
+    let toinen = "Tänne tulee arvo";
+    console.log(toinen)
+}
+
+    //kutsu funktio näin:
+    funktio();
